35a15639badc88695916132cf6ec582836791013
Chalila Nurdiana <fadya@chalo-2.local>
2025-11-19 10:23:00 +0700

    finishing login UI definition

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,45 @@
+import 'package:attendance_app/services/auth_services.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _authService = AuthServices();
+  bool _isLoading = false;
+  bool _obscurePassword = true;
+
+  Future<void> _login() async {
+    if (!_formKey.currentState!.validate()) return;
+
+    setState(() => _isLoading = true);
+
+    try {
+      await _authService.signInWithEmailAndPassword(
+        _emailController.text.trim(),
+        _passwordController.text.trim()
+      );
+    } catch (e) {
+      if (mounted) { 
+        //kalo error
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("an error occured, please try again later"), backgroundColor: Colors.red)
+        );
+      }
+    } finally {
+      if (mounted) setState(() => _isLoading = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
